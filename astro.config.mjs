5c224e2bbfdda171606ccfe26a2de071578640a2
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2026-06-30 18:04:44 -0300

    chore: remove deprecated config warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
